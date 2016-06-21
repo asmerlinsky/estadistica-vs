b0ff71d12a22480d363d552624b8c246637a9b3d
asmerlinsky <agusan1991@gmail.com>
2016-06-21 15:00:45 -0300

    agregue los archivos de analisis resultados que faltaban actualizar

--- analisis-resultados/analisis.py
+++ analisis-resultados/analisis.py
@@ -2,33 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
-#resultados=np.genfromtxt('resultados.dat',dtype=None)
+resultados=np.genfromtxt('resultados.dat',names=True,dtype=[('f0', '<i4'), ('f1', '<f8'), ('f2', 'S100'),('f3', 'S20'), ('f4', '<i4')])
+#labels = np.genfromtxt('resultados.dat', delimiter='\t', usecols=[2],dtype=None)
+
+#values = np.loadtxt('resultados.dat', delimiter='\t', usecols=[0,1,3,4],dtype=None)
+# bincan=np.bincount(resultados['3'])#para esto tengo que numerar las columnas de resultados.dat
+
+# plt.figure()
+# plt.title()
+# plt.hist(resultados['3'],range=(11,27),bins=(27-11))
+listaarchivos=np.unique(resultados['2'])
+templados=np.unique(resultados['3'])
+dicttemp=dict.fromkeys(templados.tolist(),0)
+dicttemparch=dict.fromkeys(templados.tolist(),0)
 
 
-labels = np.genfromtxt('resultados.dat', delimiter='\t', usecols=[2],dtype=None)
-values = np.loadtxt('resultados.dat', delimiter='\t', usecols=[0,1,3,4])
-
-for i in range(1,3):
-    for j in range(1,8):
-       if int(str(i)+str(j)) in values[:,2]:
-           print("templado "+str(i)+str(j)+" correlacionó alguna vez")
-       else:  
-           print("templado "+str(i)+str(j)+" no correlacionó")
+for archivo in listaarchivos:
+    for key,values in dicttemp.items():
+        try:
+            dicttemp[key]+=resultados['4'][np.logical_and((resultados['2']==archivo),(resultados['3']==key))][-1]
+            dicttemparch[key]=resultados['4'][np.logical_and((resultados['2']==archivo),(resultados['3']==key))][-1]
+        except:
+            dicttemparch[key]=0
             
 
-#listacorr=glob.glob("corremg26*.Sound.dat")
-#print("la cantidad de corremg26 es",len(listacorr))
-#maximos=np.zeros(len(listacorr))
-#mediaabs=np.zeros(len(listacorr))
-#for i in range(0,len(listacorr)):
-#    a=np.loadtxt(listacorr[i])[:,1]
-#    maximos[i]=np.max(a)
-#    mediaabs[i]=np.mean(np.abs(a))
-#
-#print("máxima correlación con emg 26 es",np.max(maximos))
-#print("correlacion media con emg 26 es",np.mean(mediaabs))
+    cantsil=sum(1 for keys in dicttemparch if dicttemparch[keys]!=0)
+        
+    print('El archivo {} correlaciona con {} templados '.format(archivo,cantsil))
 
-j=0
-a=np.unique(labels
-for i in range(0,len(labels)):
-    
+dicttemp_string = dict([(k.decode("utf-8"), v) for k, v in dicttemp.items()])
+valores=[dicttemp_string[key] for key in sorted(dicttemp_string.keys())]
+
+plt.bar(range(len(dicttemp_string)), valores, align='center')
+plt.xticks(range(len(dicttemp_string)), sorted(dicttemp_string.keys()))
+
+
+plt.show(block=True)
--- analisis-resultados/figuras-todas-correlaciones-v2.py
+++ analisis-resultados/figuras-todas-correlaciones-v2.py
@@ -11,7 +11,7 @@ import os
 import glob
 
 
-def consecutive(data, stepsize=1):
+def consecutivefirst(data, stepsize=1):
     s=np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
     if len(s[0])>0:
         t = tuple(x[0] for x in s)
@@ -22,55 +22,34 @@ def consecutive(data, stepsize=1):
 
 files=glob.glob('datos/ZF*.Sound')
 
-resultados=np.genfromtxt('resultados.dat',names=True,dtype=[('f0', '<i4'), ('f1', '<f8'), ('f2', 'S100'),('f3', '<i4'), ('f4', '<i4')])
-silabas=np.unique(resultados['3'])
-poslabel=np.linspace(0,3.1415,len(silabas))
+resultados=np.genfromtxt('resultados.dat',names=True,dtype=[('f0', '<i4'), ('f1', '<f8'), ('f2', 'S100'),('f3', 'S20'), ('f4', '<i4')])
+templados=np.unique(resultados['3'])
+poslabel=np.linspace(np.pi/len(templados),2*np.pi-np.pi/len(templados),len(templados))
 np.random.shuffle(poslabel)
 poslabelx=np.cos(poslabel)
 poslabely=np.sin(poslabel)
 
 for archivo in files:
     data=np.loadtxt(archivo)
+    tiempo=np.linspace(0,len(data)/44150,len(data))
     plt.figure()
     plt.title(os.path.basename(archivo))
-    plt.plot(data)
-
-    for i in range(0,int(len(silabas)/2)):
+    plt.plot(tiempo,data)
+    i=0
+    for key in templados: #i in range(0,int(len(silabas)/2)):
+        i+=1
         nomfile=bytes('envolvente.'+os.path.basename(archivo)+'.dat',encoding="UTF-8")
         try:
-            plt.plot(resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])],resultados['1'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])],ms=8,marker='o',label='temp 1'+str(i+1),ls='None')
+            plt.plot(tiempo[resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==key)]],resultados['1'][np.logical_and(resultados['2']==nomfile,resultados['3']==key)],ms=8,marker='o',label='temp'+key.decode("utf-8"),ls='None')
 
 
-            lugares=consecutive(resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])])
+            lugares=consecutivefirst(resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==key)])
             for lugar in lugares:
-                resultado=resultados['1'][np.logical_and(np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i]),resultados['0']==lugar)][0]
-                
-                #if (i % 2 == 0):
-                plt.annotate('S'+str(silabas[i]), xy = (lugar, resultado), xytext = (lugar+5000*poslabelx[i], resultado+0.15*poslabely[i]),arrowprops=dict(facecolor='black', shrink=0.05,fc='b'))        
-                #else:
-                #    plt.annotate('S'+str(silabas[i]), xy = (lugar, resultado), xytext = (lugar-3000, resultado-.1),arrowprops=dict(facecolor='black', shrink=0.05))      
+                resultado=resultados['1'][np.logical_and(np.logical_and(resultados['2']==nomfile,resultados['3']==key),resultados['0']==lugar)][0]
+                plt.annotate('S'+key.decode("utf-8"), xy = (tiempo[lugar], resultado), xytext = (tiempo[lugar]+(5000/44150)*poslabelx[i], resultado+0.15*poslabely[i]),arrowprops=dict(facecolor='black', shrink=0.05,fc='b'))        
+                 
         except:
             pass
-    
-    
-    plt.figure()
-    plt.title(os.path.basename(archivo))
-    plt.plot(data)
-    for i in range(int(len(silabas)/2),len(silabas)):
-        nomfile=bytes('envolvente.'+os.path.basename(archivo)+'.dat',encoding="UTF-8")
-        try:
-            plt.plot(resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])],resultados['1'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])],ms=8,marker='o',label='temp 1'+str(i+1),ls='None')
-
-
-            lugares=consecutive(resultados['0'][np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i])])
-            for lugar in lugares:
-                resultado=resultados['1'][np.logical_and(np.logical_and(resultados['2']==nomfile,resultados['3']==silabas[i]),resultados['0']==lugar)][0]
-                
-                #if (i % 2 == 0):
-                plt.annotate('S'+str(silabas[i]), xy = (lugar, resultado), xytext = (lugar+5000*poslabelx[i], resultado+0.15*poslabely[i]),arrowprops=dict(facecolor='black', shrink=0.05,fc='b'))        
-                #else:
-                #    plt.annotate('S'+str(silabas[i]), xy = (lugar, resultado), xytext = (lugar-3000, resultado-.1),arrowprops=dict(facecolor='black', shrink=0.05))      
-        except:
-            pass
+  
 
 plt.show(block=True)
